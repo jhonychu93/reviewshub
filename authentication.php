64edaba99b4cb2218a451f5a902553c489123eb5
jhonychu93 <71391124+jhonychu93@users.noreply.github.com>
2022-12-13 22:52:12 +0100

    Create authentication.php

---
+++ authentication.php
@@ -0,0 +1,27 @@
+<?php    
+    echo "Success1"
+    include('connection.php');  
+    echo "Success"
+    $username = $_POST['user'];  
+    $password = $_POST['pass'];  
+      
+        //to prevent from mysqli injection  
+        $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = mysqli_real_escape_string($con, $password);  
+      
+        $sql = "select *from login where username = '$username' and password = '$password'";  
+        $queryRecords = pg_query($this->conn, $sql) or die("error to fetch employees data");
+        $result = pg_fetch_all($sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo "<h1><center> Login successful </center></h1>";  
+        }  
+        else{  
+            echo "<h1> Login failed. Invalid username or password.</h1>";  
+        }  
+
+?>  
